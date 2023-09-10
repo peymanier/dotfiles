57f83541d78b6a97b25d3ba8ec961ecfeffcfec1
Peyman <hassani.peyman627@gmail.com>
2023-09-10 16:55:54 +0330

    Add new nvim minimal setup

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cat ~/.config/nvim/init.lua > init.lua
-cat ~/.zshrc > .zshrc
-cat ~/.tmux.conf > .tmux.conf
-cat ~/.ideavimrc > .ideavimrc
+# cat ~/.config/nvim/init.lua > init.lua
+cat ~/.config/nvim/init.lua > minimal_init.lua
+cat ~/.zshrc > zshrc
+cat ~/.tmux.conf > tmux.conf
+cat ~/.ideavimrc > ideavimrc

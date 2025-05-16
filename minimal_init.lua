501b18c752b9f93a0ed40b0529c882ac0f51185a
Peyman <hassani.peyman627@gmail.com>
2025-05-16 14:33:00 +0330

    Add new minimal lua setup

--- minimal_init.lua
+++ minimal_init.lua
@@ -1,53 +1,79 @@
-require("packer").startup(function(use)
-    use { "wbthomason/packer.nvim" }
-    use { "folke/tokyonight.nvim" }
-    use { 'm4xshen/autoclose.nvim' }
-end)
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
 
--- autoclose
-require("autoclose").setup()
-
--- colorscheme
-vim.cmd("colorscheme tokyonight")
-
--- options
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
-vim.opt.clipboard = "unnamedplus"
+-- Options
+vim.opt.nu = true
+vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
 vim.opt.smartindent = true
-vim.opt.breakindent = true
-vim.opt.mouse = 'a'
+
 vim.opt.wrap = false
-vim.opt.scrolloff = 8
-vim.opt.colorcolumn = "120"
-vim.opt.signcolumn = "yes"
-vim.opt.termguicolors = true
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-vim.opt.ignorecase = true
-
-vim.opt.undofile = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
-vim.opt.timeout = true
-vim.opt.timeoutlen = 500
+
 vim.opt.updatetime = 50
 
--- remaps
-vim.keymap.set("i", "jj", "<Esc>")
-vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set("n", "<leader>cc", ":noh<CR>")
+vim.opt.colorcolumn = "80"
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.opt.mouse = 'a'
+vim.opt.clipboard = "unnamedplus"
+
+-- Keymaps
+vim.keymap.set("i", "jj", "<ESC>", { desc = "exit insert mode" })
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("n", "<CR>", "o<ESC>", { noremap = true, silent = true })
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- add your plugins here
+    { "rose-pine/neovim", name = "rose-pine" }
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "rose-pine" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+-- Colorscheme
+vim.cmd.colorscheme("rose-pine")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
